81d687894db2f48f1cd839dfc1a052caa749c79d
lpkyrius <lpkyrius@hotmail.com>
2024-11-07 15:38:32 +0000

    Finished parameters

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from fastapi import APIRouter
+from typing import Optional, List, Dict
+from fastapi import APIRouter, Query, Body, Path
 from pydantic import BaseModel
 
 router = APIRouter(
@@ -7,15 +7,50 @@ router = APIRouter(
     tags=['blog']
 )
 
+class Image(BaseModel):
+   url: str
+   alias: str
 class BlogModel(BaseModel):
-    title: str
-    content: str
-    published: Optional[bool]
+  title: str
+  content: str
+  nb_comments: int
+  published: Optional[bool]
+  tags: List[str] = []
+  metadata: Dict[str, str] = {'key1': 'val1'}
+  image: Optional[Image] = None
 
 @router.post('/new/{id}')
-def create_blog(blog: BlogModel, id: int, version: int = 1):
+def create_blog(blog: BaseModel, id: int, version: int = 1):
+  return {
+    'id': id,
+    'data': blog,
+    'version': version
+    }
+
+@router.post('/new/{id}/comment/{comment_id}')
+def create_comment(blog: BlogModel, id: int, 
+        comment_title: int = Query(None,
+            title='Title of the comment',
+            description='Some description for comment_title',
+            alias='commentTitle',
+            deprecated=True
+        ),
+        # content: str = Body('hi how are you')
+        # content: str = Body(Ellipsis)
+        content: str = Body(...,
+            min_length=10,
+            max_length=50,
+            regex='^[a-z\\s]*$' # only lowercase letters/space
+        ),
+        # v: Optional[List[str]] = Query(None)
+        v: Optional[List[str]] = Query(['1.0', '1.1', '1.2']),
+        comment_id: int = None  # Path(None, gt=5, le=10)
+    ):
     return {
+        'blog': blog,
         'id': id,
-        'data': blog,
-        'version': version
-        }
+        'comment_title': comment_title,
+        'content': content,
+        'version': v,
+        'comment_id': comment_id
+    }
